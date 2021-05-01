81212bd56b18eeeae90b02c6e3a4824a6d717e49
Waqqars987 <waqqar@magicmindtechnologies.com>
2021-05-01 14:11:59 +0530

    completed api routes section

---
+++ nextjs-and-react-the-complete-guide/8. adding-backend-code-wtih-api-routes/pages/api/feedback/[feedbackId].js
@@ -0,0 +1,11 @@
+import { buildFeedbackPath, extractFeedback } from './index';
+
+function handler(req, res) {
+	const feedbackId = req.query.feedbackId;
+	const filePath = buildFeedbackPath();
+	const data = extractFeedback(filePath);
+	const selectedFeedback = data.find(item => item.id === feedbackId);
+	res.status(200).json({ feedback: selectedFeedback });
+}
+
+export default handler;
