173794c84ced5479c8fde19d84f24b65986abbaa
Waqqars987 <Waqqars987@gmail.com>
2024-02-13 15:18:03 +0530

    progress on app router section

--- nextjs-and-react-the-complete-guide/3.2. onwards-foodies/app/meals/page.js
+++ nextjs-and-react-the-complete-guide/3.2. onwards-foodies/app/meals/page.js
@@ -1,5 +1,38 @@
+import Link from 'next/link';
+import { Suspense } from 'react';
+
+import { getMeals } from '@/lib/meals';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
+async function Meals() {
+	const meals = await getMeals();
+
+	return <MealsGrid meals={meals} />;
+}
+
 function MealsPage() {
-	return <h1>Meals Page</h1>;
+	return (
+		<>
+			<header className={classes.header}>
+				<h1>
+					Delicious meals, created <span className={classes.highlight}>by you</span>
+				</h1>
+				<p>Choose your favorite receipe and cook it yourself. It is easy and fun!</p>
+				<p className={classes.cta}>
+					<Link href='/meals/share'>Share Your Favorite Recipe</Link>
+				</p>
+			</header>
+
+			<main className={classes.main}>
+				{/* handle loading state at a granular level */}
+				{/* loading.js files internally wrap the entire page content in suspense */}
+				<Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+					<Meals />
+				</Suspense>
+			</main>
+		</>
+	);
 }
 
 export default MealsPage;
--- nextjs-and-react-the-complete-guide/3.2. onwards-foodies/components/main-header/main-header.js
+++ nextjs-and-react-the-complete-guide/3.2. onwards-foodies/components/main-header/main-header.js
@@ -1,6 +1,9 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
+import NavLink from './nav-link';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from './main-header-background';
@@ -26,10 +29,10 @@ function MainHeader() {
 				<nav className={classes.nav}>
 					<ul>
 						<li>
-							<Link href='/meals'>Browse Meals</Link>
+							<NavLink href='/meals'>Browse Meals</NavLink>
 						</li>
 						<li>
-							<Link href='/community'>Foodies Community</Link>
+							<NavLink href='/community'>Foodies Community</NavLink>
 						</li>
 					</ul>
 				</nav>
