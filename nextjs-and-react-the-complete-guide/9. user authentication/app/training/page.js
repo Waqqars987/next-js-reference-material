209c0035c3079e5ce96502070c3a64b361ff099e
Waqqars987 <Waqqars987@gmail.com>
2024-10-29 13:42:08 +0530

    start section 9

--- nextjs-and-react-the-complete-guide/9. user authentication/app/training/page.js
+++ nextjs-and-react-the-complete-guide/9. user authentication/app/training/page.js
@@ -1,22 +1,32 @@
 import { getTrainings } from '@/lib/training';
+import Image from 'next/image';
 
 export default async function TrainingPage() {
-  const trainingSessions = getTrainings();
+	const trainingSessions = getTrainings();
 
-  return (
-    <main>
-      <h1>Find your favorite activity</h1>
-      <ul id="training-sessions">
-        {trainingSessions.map((training) => (
-          <li key={training.id}>
-            <img src={`/trainings/${training.image}`} alt={training.title} />
-            <div>
-              <h2>{training.title}</h2>
-              <p>{training.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+	return (
+		<main>
+			<h1>Find your favorite activity</h1>
+			<ul id='training-sessions'>
+				{trainingSessions.map(training => (
+					<li key={training.id}>
+						<Image
+							src={`/trainings${training.image}`}
+							alt={training.title}
+							width={202}
+							height={202}
+							priority
+							style={{
+								height: 'auto'
+							}}
+						/>
+						<div>
+							<h2>{training.title}</h2>
+							<p>{training.description}</p>
+						</div>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
 }
