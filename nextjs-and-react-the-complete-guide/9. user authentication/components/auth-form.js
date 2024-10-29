589df0f3eb3f3f82ee964c591def1b73a5586149
Waqqars987 <Waqqars987@gmail.com>
2024-10-29 15:53:23 +0530

    Completed section 9

--- nextjs-and-react-the-complete-guide/9. user authentication/components/auth-form.js
+++ nextjs-and-react-the-complete-guide/9. user authentication/components/auth-form.js
@@ -1,8 +1,15 @@
-import Link from 'next/link';
+'use client';
 
-export default function AuthForm() {
+import Link from 'next/link';
+import { useFormState } from 'react-dom';
+
+import { auth } from '@/actions/auth-actions';
+
+export default function AuthForm({ mode }) {
+	const [formState, formAction] = useFormState(auth.bind(null, mode), {});
+
 	return (
-		<form id='auth-form'>
+		<form id='auth-form' action={formAction}>
 			<div>
 				<img src='/images/auth-icon.jpg' alt='A lock icon' />
 			</div>
@@ -14,11 +21,22 @@ export default function AuthForm() {
 				<label htmlFor='password'>Password</label>
 				<input type='password' name='password' id='password' />
 			</p>
+
+			{formState.errors && (
+				<ul id='form-errors'>
+					{Object.keys(formState.errors).map(error => (
+						<li key={error}>{formState.errors[error]}</li>
+					))}
+				</ul>
+			)}
+
 			<p>
-				<button type='submit'>Create Account</button>
+				<button type='submit'>{mode === 'login' ? 'Login' : 'Create Account'}</button>
 			</p>
 			<p>
-				<Link href='/'>Login with existing account.</Link>
+				{mode === 'login' && <Link href='/?mode=signup'>Create an account.</Link>}
+
+				{mode === 'signup' && <Link href='/?mode=login'>Login with existing account.</Link>}
 			</p>
 		</form>
 	);
